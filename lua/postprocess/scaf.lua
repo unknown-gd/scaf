9afed0bb6b509aae84ce5fc60fd503e78645aa3b
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2024-02-14 01:57:46 +0400

    Small changes and few improvements

--- lua/postprocess/scaf.lua
+++ lua/postprocess/scaf.lua
@@ -1,17 +1,18 @@
 local addonName = "Simple Chromatic Aberration Filter"
 
--- ---------------------------------------------------v  set true=Allow ConVar saving, false=the opposite
+-- https://wiki.facepunch.com/gmod/Global.CreateClientConVar
 local pp_scaf = CreateClientConVar( "pp_scaf", "0", true, false, "Enable/Disable chromatic aberration filter.", 0, 1 )
 local pp_scaf_intensity = CreateClientConVar( "pp_scaf_intensity", "2", true, false, "How intense the chromatic aberration will be.", 0, 100 )
 
-local pp_scaf_redx = CreateClientConVar( "pp_scaf_redx", "8", true, false, "Mixing of chromatic aberrations in the red channel along the X-axis. <Default 8>", 0, 100 )
-local pp_scaf_redy = CreateClientConVar( "pp_scaf_redy", "4", true, false, "Mixing of chromatic aberrations in the red channel along the Y-axis. <Default 4>", 0, 100 )
+-- https://i.imgur.com/8B3KakB.png
+local pp_scaf_redx = CreateClientConVar( "pp_scaf_redx", "8", true, false, "Mixing of chromatic aberrations in the red channel along the X-axis.", 0, 128 )
+local pp_scaf_redy = CreateClientConVar( "pp_scaf_redy", "4", true, false, "Mixing of chromatic aberrations in the red channel along the Y-axis.", 0, 128 )
 
-local pp_scaf_greenx = CreateClientConVar( "pp_scaf_greenx", "4", true, false, "Mixing of chromatic aberrations in the green channel along the X-axis. <Default 4>", 0, 100 )
-local pp_scaf_greeny = CreateClientConVar( "pp_scaf_greeny", "2", true, false, "Mixing of chromatic aberrations in the green channel along the Y-axis. <Default 2>", 0, 100 )
+local pp_scaf_greenx = CreateClientConVar( "pp_scaf_greenx", "4", true, false, "Mixing of chromatic aberrations in the green channel along the X-axis.", 0, 128 )
+local pp_scaf_greeny = CreateClientConVar( "pp_scaf_greeny", "2", true, false, "Mixing of chromatic aberrations in the green channel along the Y-axis.", 0, 128 )
 
-local pp_scaf_bluex = CreateClientConVar( "pp_scaf_bluex", "0", true, false, "Mixing of chromatic aberrations in the blue channel along the X-axis. <Default 0>", 0, 100 )
-local pp_scaf_bluey = CreateClientConVar( "pp_scaf_bluey", "0", true, false, "Mixing of chromatic aberrations in the blue channel along the Y-axis. <Default 0>", 0, 100 )
+local pp_scaf_bluex = CreateClientConVar( "pp_scaf_bluex", "0", true, false, "Mixing of chromatic aberrations in the blue channel along the X-axis.", 0, 128 )
+local pp_scaf_bluey = CreateClientConVar( "pp_scaf_bluey", "0", true, false, "Mixing of chromatic aberrations in the blue channel along the Y-axis.", 0, 128 )
 
 list.Set( "PostProcess", "#pp_scaf.name", {
 	["icon"] = "gui/postprocess/scaf.jpg",
@@ -128,22 +129,22 @@ end, addonName )
 
 local Run = hook.Run
 
+local function isPostProcessPermitted()
+	return enabled and Run( "PostProcessPermitted", "scafilter" ) ~= false
+end
+
 local redX, greenX, blueX = 0, 0, 0
 local redY, greenY, blueY = 0, 0, 0
+local floor = math.floor
+local intensity = 0
 
-do
-
-	local floor = math.floor
-	local intensity = 0
-
-	hook.Add( "Think", addonName, function()
-		if not enabled or Run( "PostProcessPermitted", "scafilter" ) == false then return end
+hook.Add( "Think", addonName, function()
+	if isPostProcessPermitted() then
 		intensity = pp_scaf_intensity:GetFloat()
 		redX, greenX, blueX = floor( pp_scaf_redx:GetInt() * intensity ), floor( pp_scaf_greenx:GetInt() * intensity ), floor( pp_scaf_bluex:GetInt() * intensity )
 		redY, greenY, blueY = floor( pp_scaf_redy:GetInt() * intensity ), floor( pp_scaf_greeny:GetInt() * intensity ), floor( pp_scaf_bluey:GetInt() * intensity )
-	end )
-
-end
+	end
+end )
 
 -- https://wiki.facepunch.com/gmod/GM:OnScreenSizeChanged
 local width, height = ScrW(), ScrH()
@@ -152,28 +153,32 @@ hook.Add( "OnScreenSizeChanged", addonName, function()
 end )
 
 -- https://gitspartv.github.io/LuaJIT-Benchmarks/#test1
-local UpdateScreenEffectTexture, GetScreenEffectTexture = render.UpdateScreenEffectTexture, render.GetScreenEffectTexture
-local SetMaterial, DrawScreenQuad, DrawScreenQuadEx = render.SetMaterial, render.DrawScreenQuad, render.DrawScreenQuadEx
-local red, green, blue = Material( "color/red" ), Material( "color/green" ), Material( "color/blue" )
-local black = Material( "vgui/black" )
+local SetMaterial, DrawScreenQuad, DrawScreenQuadEx, UpdateScreenEffectTexture = render.SetMaterial, render.DrawScreenQuad, render.DrawScreenQuadEx, render.UpdateScreenEffectTexture
+local screenEffectTexture, black = render.GetScreenEffectTexture( 0 ), Material( "vgui/black" )
+
+local red = Material( "color/red" )
+red:SetTexture( "$basetexture", screenEffectTexture )
+
+local green = Material( "color/green" )
+green:SetTexture( "$basetexture", screenEffectTexture )
+
+local blue = Material( "color/blue" )
+blue:SetTexture( "$basetexture", screenEffectTexture )
 
 hook.Add( "RenderScreenspaceEffects", addonName, function()
-	if not enabled or Run( "PostProcessPermitted", "scafilter" ) == false then return end
-	UpdateScreenEffectTexture()
+	if isPostProcessPermitted() then
+		UpdateScreenEffectTexture()
 
-	red:SetTexture( "$basetexture", GetScreenEffectTexture() )
-	green:SetTexture( "$basetexture", GetScreenEffectTexture() )
-	blue:SetTexture( "$basetexture", GetScreenEffectTexture() )
+		SetMaterial( black )
+		DrawScreenQuad()
 
-	SetMaterial( black )
-	DrawScreenQuad()
+		SetMaterial( red )
+		DrawScreenQuadEx( -redX / 2, -redY / 2, width + redX, height + redY )
 
-	SetMaterial( red )
-	DrawScreenQuadEx( -redX / 2, -redY / 2, width + redX, height + redY )
+		SetMaterial( green )
+		DrawScreenQuadEx( -greenX / 2, -greenY / 2, width + greenX, height + greenY )
 
-	SetMaterial( green )
-	DrawScreenQuadEx( -greenX / 2, -greenY / 2, width + greenX, height + greenY )
-
-	SetMaterial( blue )
-	DrawScreenQuadEx( -blueX / 2, -blueY / 2, width + blueX, height + blueY )
+		SetMaterial( blue )
+		DrawScreenQuadEx( -blueX / 2, -blueY / 2, width + blueX, height + blueY )
+	end
 end )
